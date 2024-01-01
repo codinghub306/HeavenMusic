68e6a29f324e4b63e636fb4cfb59c7aa26c5b274
Heaven <154735143+codinghub306@users.noreply.github.com>
2024-01-01 08:31:27 +0530

    Update play.py

--- HeavenMusic/utils/inline/play.py
+++ HeavenMusic/utils/inline/play.py
@@ -25,20 +25,6 @@ def track_markup(_, videoid, user_id, channel, fplay):
         ],
     ]
    
-def stream_markup(_, chat_id):
-    buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="♡︎", callback_data=f"ADMIN Playlist|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-           InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")] ,
-   ],
-    return buttons
-
 
 def stream_markup(_, chat_id):
     buttons = [
